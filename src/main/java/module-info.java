7dfbe5ec5f16ad62104b8b926ddb351d028b48ea
Zerneax <d.chris59@gmail.com>
2019-05-11 18:35:08 +0200

    Add some stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.javafx {
     requires javafx.fxml;
 
     opens org.javafx to javafx.fxml;
+    opens org.javafx.controller to javafx.fxml;
     exports org.javafx;
     exports org.javafx.controller;
 }
--- src/main/java/org/javafx/controller/XplorerController.java
+++ src/main/java/org/javafx/controller/XplorerController.java
@@ -1,7 +1,66 @@
 package org.javafx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class XplorerController {
 
+    @FXML
+    private TextField hostField;
+
+    @FXML
+    private TextField portField;
+
+    @FXML
+    private Button btnConnect;
+
+    @FXML
+    private Label bannerError;
+
+    @FXML
+    private TextArea visualize;
+
+    @FXML
+    protected void connect(ActionEvent event) {
+        this.bannerError.setVisible(false);
+        if(this.hostField.getText().isEmpty()) {
+            //showAlert("The Host is empty !");
+            this.bannerError.setText("The Host is empty !");
+            this.bannerError.setVisible(true);
+            return;
+        }
+
+        if(this.portField.getText().isEmpty()) {
+            //showAlert("The Port is empty !");
+            this.bannerError.setText("The Port is empty !");
+            this.bannerError.setVisible(true);
+            return;
+        }
+
+        for(int i = 0; i < 100; i ++) {
+            this.visualize.setText(this.visualize.getText() + "\n Test" );
+        }
+
+        //this.visualize.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        Region contentTextArea = (Region) this.visualize.lookup(".content");
+        contentTextArea.setStyle("-fx-background-color: white;");
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Form Error !");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        //alert.initOwner(this.btnConnect.getScene().getWindow());
+        alert.showAndWait();
+    }
 }
